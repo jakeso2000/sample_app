22ae09f82d2ca6903b6b1ef845a3cbf8318ffedc
jakeso2000 <aramakikuniharu@AramakiKuniharu-no-MacBook-Air.local>
2013-10-23 20:23:57 +0900

    Finish user edit, update, index, and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,14 @@
 class SessionsController < ApplicationController
 
  def new
+ 	
  end
 
  def create
  	user = User.find_by_email(params[:session][:email].downcase)
  	if user && user.authenticate(params[:session][:password])
+ 		sign_in user
+ 		redirect_back_or user
  	else
  	    flash.now[:error] = 'Invalid email/password combination' #誤りあり！
  	    render 'new'
@@ -13,7 +16,7 @@ class SessionsController < ApplicationController
  end
 
  def destroy
- 	 sign_out
+ 	 sign_out 
  	 redirect_to root_url
  end
 
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -6,12 +6,23 @@ describe "User pages" do
 
   subject { page }
 
+  describe "edit" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit edit_user_path(user) }
+
+    describe "page" do
+      it { should have_selector('h1',    text: "Update your profile") }
+      it { should have_selector('title', text: "Edit user") }
+      it { should have_link('change', href: 'http://gravatar.com/emails') }
+    end
+
   describe "profile page" do
     let(:user){FactoryGirl.create(:user)}
      before {visit user_path(user)}
 
     it{should have_selector('h1', text: user.name)}
     it{should have_selector('title', text: user.name)}
+
   end
 
   describe "signup" do 
@@ -23,6 +34,8 @@ describe "User pages" do
         expect {click_button submit}.not_to change(User, :count)
       end
     end
+
+
 
     describe "with valid information" do
       before do 
