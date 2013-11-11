9aeface84f4073a163011313740cf91c20f834a7
jakeso2000 <aramakikuniharu@AramakiKuniharu-no-MacBook-Air.local>
2013-11-11 23:06:14 +0900

    Add user following

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -28,6 +28,13 @@ describe User do
   it { should respond_to(:remember_token)}
   it { should respond_to(:authenticate)}
   it { should respond_to(:microposts)}
+  it { should respond_to(:feed)}
+  it { should respond_to(:relationships)}
+  it { should respond_to(:followed_users)}
+  it { should respond_to(:following?)}
+  it { should respond_to(:follow)}
+  it { should respond_to(:unfollow!)}
+
  it {should be_valid}
 
 
@@ -118,4 +125,22 @@ end
   its(:remember_token){ should_not be_blank}
  end
 
+ describe "following" do
+  let(:other_user){FactoryGirl.create(:user)}
+  before do
+    @user.save
+    @user.follow!(other_user)
+  end
+
+   describe "and unfollowing" do
+    before {@user.unfollow!(other_user)}
+
+    it{ should_not be_following(other_user)}
+    its(:followed_users){ should_not include(other_user)}
+  end
+
+  it {should be_following(other_user)}
+  its(:followed_users){ should include(other_user)}
+  end
+ 
 end
